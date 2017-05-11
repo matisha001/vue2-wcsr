83df228be64eb279bedd2a3864170ac71f582d0f
matisha <matisha001@sina.com>
2017-05-11 16:26:45 +0800

    m

--- src/main.js
+++ src/main.js
@@ -2,14 +2,13 @@ import 'lib-flexible'
 import FastClick from 'fastclick'
 window.FastClick = FastClick
 import Vue from 'vue'
-import VueResource from 'vue-resource'
-Vue.use(VueResource)
-
+// import VueResource from 'vue-resource'
+// Vue.use(VueResource)
 import filters from '@/tools/filters'
 Object.keys(filters).forEach((k) => Vue.filter(k, filters[k]))
-// import App from './router/list.vue'
 
-import App from '@/view/App'
+
+// import App from '@/view/App'
 import router from '@/router'
 
 Vue.config.productionTip = false
